8bb03f73bc45b03097f9b7e47d123bcf89b6f5c7
chhotukumarom93-glitch <chhotukumarom93@gmail.com>
2026-04-08 11:55:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -146,7 +146,8 @@ def ads():
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory('static', 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 # -----------------------------
 # ROUTES
 # -----------------------------
